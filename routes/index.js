8997922b0a25abc0c95d3f901257ca6d82be2736
Eric Glasser <eric@ericglasser.com>
2016-02-19 00:18:11 -0700

    added sitemap and trobots

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,12 @@ exports = module.exports = function(app) {
 	app.get('/team', routes.views.index);
 	app.get('/contact', routes.views.index);
 	app.get('/views/:viewFile.html', routes.views.view);
+	app.get('/sitemap.xml', function (req, res) {
+		res.sendfile('sitemap.xml', {root: './public'});
+	});
+	app.get('/robots.txt', function (req, res) {
+		res.sendfile('robots.txt', {root: './public'});
+	});
 	//app.get('/blog/:category?', routes.views.blog);
 	//app.get('/blog/post/:post', routes.views.post);
 	//app.all('/contact', routes.views.contact);
